17828e12717e3cf020e015820cdbf701af72ad20
F-421 <qq67378@umbc.edu>
2022-02-16 20:44:51 -0500

    Added GameOver Screen

---
+++ CastleDrift/Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOverScreen : MonoBehaviour
+{
+
+    //restarts the track when pushed
+    [SerializeField] UnityEngine.UI.Button reset_button;
+
+    //button to quit when pushed
+    [SerializeField] UnityEngine.UI.Button quit_button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        reset_button.onClick.AddListener(ResetTrack);
+        quit_button.onClick.AddListener(QuitGame);
+
+        //hide this until we completed all laps
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void ResetTrack(){
+        Time.timeScale = 1;
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    //Exit the application (saving later)
+    void QuitGame(){
+        Application.Quit();
+
+    }
+}
--- CastleDrift/Assets/Scripts/LapController.cs
+++ CastleDrift/Assets/Scripts/LapController.cs
@@ -6,9 +6,12 @@ using TMPro;
 // this is to controll the game (and so we eventually have a stopping point)
 public class LapController : MonoBehaviour
 {
+
     public List<CheckpointController> checkpoints;
     public int totalLaps;
-    const string TAG_COMPARE = "Player"; 
+    const string TAG_COMPARE = "Player";  
+
+    public GameObject gameOver; //I couldn't get activation by tag working
 
     //update the text displaying lap number
     [SerializeField] TextMeshProUGUI lapText;
@@ -30,10 +33,15 @@ public class LapController : MonoBehaviour
                 //now we need to adjust the lap text
                 lapText.text = player.lapNum + "/" + totalLaps;
                 Debug.Log("Lap " + player.lapNum + "/" + totalLaps);
-
+ 
                 // do we end game?
                 if(player.lapNum > totalLaps){
                     Debug.Log("Game End here");
+
+                    // show end game screen (and freeze time)
+                    Time.timeScale = 0;
+                    gameOver.gameObject.SetActive(true);
+                    lapText.gameObject.SetActive(false);
                 }
             }
         }
