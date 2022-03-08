ef0cd45c875d6c83b0fd5c513f57b71ab8f2d281
vyeh1 <99453572+vyeh1@users.noreply.github.com>
2022-03-07 22:51:17 -0500

    Imported sound files

--- CastleDrift/Assets/Scripts/AudioManager.cs
+++ CastleDrift/Assets/Scripts/AudioManager.cs
@@ -25,7 +25,7 @@ public class AudioManager : MonoBehaviour
         Sound s = Array.Find(sounds, sound => sound.name == name);
         if (s == null)
         {
-            Debug.LogWarning("Sound " + name + "does not exist, did you misspell it?")
+            Debug.LogWarning("Sound " + name + "does not exist, did you misspell it?");
             return;
         }
 
