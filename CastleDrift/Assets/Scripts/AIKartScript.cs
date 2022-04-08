dfc140a9d82d691f0e4e0e99f86cf8afbec0b40d
F-421 <qq67378@umbc.edu>
2022-04-08 11:24:53 -0400

    AIKart drives through forest!

--- CastleDrift/Assets/Scripts/AIKartScript.cs
+++ CastleDrift/Assets/Scripts/AIKartScript.cs
@@ -5,15 +5,20 @@ using UnityEngine.InputSystem;
 
 public class AIKartScript : MonoBehaviour
 {
+	public float forwardInput; // setting how much we move forward
+
 	private Controls_Player car_driver;
 	[SerializeField] private List<Transform> waypoints; //list of places to go on route
 	private int waypoint_i;
 	
 	private Vector3 targetPosition;
+
+	const string TAG_COMPARE = "AIWaypoint"; // how we identify that we make it to a waypoint
 	
 	private void Awake(){
 		car_driver = GetComponent<Controls_Player>();
 		waypoint_i = 0;
+		//forwardInput = 1;
 	}
 
 
@@ -25,12 +30,52 @@ public class AIKartScript : MonoBehaviour
 		Vector3 dirToMovePosition = (targetPosition - transform.position).normalized;
 		float dot = Vector3.Dot(transform.forward, dirToMovePosition);
 		
-		Debug.Log(dot);
-		
-		car_driver.SetMovement(1, 0);
+		// are we moving towards or away form our waypoint?
+		float forwardSpeed = forwardInput;
+		if (dot < 0){
+			forwardSpeed = -forwardInput;
+		}
+
+		// turn car towards waypoint
+		float angleToMove = Vector3.SignedAngle(transform.forward, dirToMovePosition, Vector3.up);
+		float turnSpeed = 0f;
+		if(angleToMove > 0){
+			turnSpeed = 1f;
+		}
+		else if(angleToMove < 0){
+			turnSpeed = -1f;
+		}
+
+		car_driver.SetMovement(forwardSpeed, turnSpeed);
+		//car_driver.SetMovement(1, 0);
 	}
 	
 	public void SetTargetPosition(Vector3 targetPosition){
 		this.targetPosition = targetPosition;
+	}
+
+	/*collided with another object*/
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag(TAG_COMPARE))
+        {
+			Debug.Log("AI Kart collided with a waypoint");
+
+			//is it our current waypoint? 
+			GameObject collidedwaypoint = other.gameObject;
+            if ((waypoints[waypoint_i] is not null) && (collidedwaypoint.transform != waypoints[waypoint_i]))
+            {
+                return;
+            }
+
+			Debug.Log("We hit the correct waypoint");
+
+			waypoint_i++;
+
+			// hit end of waypoint list: back to beginning! (this should cover a lap of track)
+			if(waypoint_i == waypoints.Count){
+				waypoint_i = 0;
+			}
+
+		}
 	}
 }
